3825799dd4161719ca091cd5f3434af3dffb0702
Rajbahadur Singh Gour <rajbahadur.singh.gour@signify.com>
2019-05-21 15:33:31 +0530

    Adding Automation Test Project to Git Repository

---
+++ AutomationSuite/com.org.api/src/test/java/APITesting/com/org/api/HelloWorld.java
@@ -0,0 +1,16 @@
+package APITesting.com.org.api;
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		for(int i =0; i<10; i++)
+		{
+			System.out.println("Hello::::");
+		}
+
+	}
+
+}
